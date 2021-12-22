97b820d1aedce049459c11e3917bfff8aead8c07
gonnavis <gonnavis@163.com>
2021-12-22 13:28:02 +0800

    show stats.

--- webaverse.js
+++ webaverse.js
@@ -81,7 +81,7 @@ export default class Webaverse extends EventTarget {
     rendererStats.domElement.style.position = 'absolute';
     rendererStats.domElement.style.left = '0px';
     rendererStats.domElement.style.bottom = '0px';
-    rendererStats.domElement.style.display = 'none';
+    // rendererStats.domElement.style.display = 'none';
     document.body.appendChild( rendererStats.domElement );
 
     this.loadPromise = Promise.all([
@@ -282,9 +282,9 @@ export default class Webaverse extends EventTarget {
     // equipmentRender.render();
 
     getComposer().render();
-    if(ioManager.debugMode) {
+    // if(ioManager.debugMode) {
       rendererStats.update(renderer);
-    }
+    // }
   }
   
   startLoop() {
