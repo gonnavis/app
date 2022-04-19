378ca2dcf486c468d42a6753ff3cc1e81508839a
gonnavis <gonnavis@163.com>
2022-04-19 15:37:29 +0800

    fix hit after die ( already in ragdoll animation ) bug.

--- world.js
+++ world.js
@@ -300,7 +300,6 @@ const _bindHitTracker = app => {
       physicsManager.disableGeometryQueries(app.npcPlayer.characterController);
       app.npcPlayer.avatar.ragdoll = true;
       setTimeout(() => {
-        debugger
         world.appManager.removeTrackedApp(app.instanceId);
       }, 5000);
     } else {
