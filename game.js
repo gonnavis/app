10a61a598f80e5abd9a8ea734cf727b437f5d776
gonnavis <gonnavis@163.com>
2022-05-06 00:58:00 +0800

    eyeballTargetEnabled sync with needsEyeTarget.

--- game.js
+++ game.js
@@ -435,9 +435,18 @@ let lastHitTimes = new WeakMap();
 const _gameUpdate = (timestamp, timeDiff) => {
   const now = timestamp;
   const renderer = getRenderer();
-  console.log(zTargeting?.focusTargetReticle?.position?.x, zTargeting?.focusTargetReticle?.position?.y, zTargeting?.focusTargetReticle?.position?.z);
   
   const localPlayer = metaversefileApi.useLocalPlayer();
+
+  if (localPlayer?.avatar) {
+    console.log(
+      localPlayer.avatar.eyeTargetEnabled,
+      localPlayer.avatar.eyeballTargetEnabled,
+      zTargeting?.focusTargetReticle?.position?.x,
+      zTargeting?.focusTargetReticle?.position?.y,
+      zTargeting?.focusTargetReticle?.position?.z,
+    );
+  }
 
   const _handlePush = () => {
     if (gameManager.canPush()) {
@@ -1009,8 +1018,12 @@ const _gameUpdate = (timestamp, timeDiff) => {
         localPlayer.avatar.eyeTarget.copy(zTargeting.focusTargetReticle.position);
         localPlayer.avatar.eyeTargetInverted = true;
         localPlayer.avatar.eyeTargetEnabled = true;
-        localPlayer.eyeballTarget.copy(zTargeting.focusTargetReticle.position);
-        localPlayer.eyeballTargetEnabled = true;
+        if (localPlayer.avatar.needsEyeTarget) {
+          localPlayer.eyeballTarget.copy(zTargeting.focusTargetReticle.position);
+          localPlayer.eyeballTargetEnabled = true;
+        } else {
+          localPlayer.eyeballTargetEnabled = false;
+        }
       } else {
         localPlayer.avatar.eyeTargetEnabled = false;
         localPlayer.eyeballTargetEnabled = false;
